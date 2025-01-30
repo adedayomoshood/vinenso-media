dcba1693598574ef443e92f982f6e182e92eb123
Victor <samuelvictoralexander@gmail.com>
2025-01-30 18:28:07 +0100

    removed unuseed image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import EverythingYouNeed from "@/components/EverythingYouNeed";
 import Hero from "@/components/Hero";
 import WhatWeDo from "@/components/WhatWeDo";
-import Image from "next/image";
 
 export default function Home() {
   return (
